9bcf6fb8efcfd70d9fd897f94c1460f31c9fa8d4
shazab-saifi <shazebsaifi92@gmail.com>
2024-11-25 17:50:04 +0530

    Completed userMiddleware

--- src/db.ts
+++ src/db.ts
@@ -3,8 +3,16 @@ import mongoose, { model, Schema } from "mongoose";
 mongoose.connect("mongodb+srv://shazebsaifi92:myfirstcluster@cluster0.z1exw.mongodb.net/secondBrain");
 
 const UserSchema = new Schema({
-    userName: {type: String, unique: true, require: true},
-    password: {type: String, unique: true, require: true}
+    userName: {type: String, unique: true, required: true},
+    password: {type: String, unique: true, required: true}
 });
 
+const ContentSchema = new Schema({
+    title: {type: String, required: true},
+    link: {type: String, required: true},
+    tags: [{type: mongoose.Types.ObjectId, ref: "Tag"}],
+    userId: {type: mongoose.Types.ObjectId, ref: "User", required: true}
+})
+
+export const ContentModel = model("Content", ContentSchema);
 export const UserModel = model("Users", UserSchema);
--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,95 @@
-import express from "express"
-import jwt from "jsonwebtoken"
-import { UserModel } from "./db";
+import express from "express";
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "./config";
+import { ContentModel, UserModel } from "./db";
+import { userMiddleware } from "./middleware";
+import { Request, Response } from "express"
 
 const app = express();
 app.use(express.json());
 
 app.post("/api/v1/signup", async (req, res) => {
-    const {userName, password} = req.body;
+    const { userName, password } = req.body;
+    try {
+        await UserModel.create({
+            userName,
+            password
+        })
 
-    await UserModel.create({
+        res.json({
+            msg: "You're signed up"
+        })
+    } catch (error) {
+        res.status(411).json({
+            msg: "User already exists!"
+        })
+    }
+})
+
+app.post("/api/v1/signin", async (req, res) => {
+    const { userName, password } = req.body;
+
+    const doesUserExists = await UserModel.findOne({
         userName,
         password
-    })
+    });
 
-    res.json({
-        msg: "You're signed up"
-    })
+    if (doesUserExists) {
+        const token = jwt.sign({
+            id: doesUserExists._id
+        }, JWT_SECRET);
+
+        res.json({ token: token });
+    } else {
+        res.status(403).json({
+            msg: "Incorrect credentials!"
+        })
+    }
+});
+
+app.post("/api/v1/content", userMiddleware, async (req: Request, res: Response) => {
+    const { type, link, title } = req.body;
+    try {
+        await ContentModel.create({
+            type,
+            link,
+            userId: req.userId,
+            title
+        })
+
+        res.json({
+            msg: "Content created"
+        })
+    } catch (error) {
+        res.status(500).json({
+            msg: `Internal server: ${error}`
+        })
+    }
 })
 
-app.post("/api/v1/signin", (req, res) => {
+app.get("/api/v1/content", userMiddleware, (req, res) => {
+    const userId = req.userId;
+    try {
+        const content = ContentModel.find({
+            userId,
+        });
 
+        if (!content) {
+            res.status(204).json({
+                msg: "No content!"
+            });
+        } else {
+            res.json({ content });
+        }
+    } catch (error) {
+        console.error("Internal server error: " + error);
+        res.status(500).json({
+            err: `Internal server ${error}`
+        });
+    }
 })
 
-app.post("/api/v1/content", (req, res) => {
-
-})
-
-app.get("/api/v1/signup", (req, res) => {
-
-})
-
-app.delete("/api/v1/signup", (req, res) => {
+app.delete("/api/v1/content", (req, res) => {
 
 })
 
@@ -41,3 +100,5 @@ app.post("/api/v1/brain/share", (req, res) => {
 app.post("/api/v1/brain/:sharelink", (req, res) => {
 
 })
+
+app.listen(3000)
